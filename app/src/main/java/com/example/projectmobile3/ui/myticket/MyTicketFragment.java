aef25c4e24ac4231988e400c017eb92bfeca3d4e
NadiaLeni <nadia712leni@gmail.com>
2024-01-19 17:27:06 +0700

    MyTicket pop up

--- app/src/main/java/com/example/projectmobile3/ui/myticket/MyTicketFragment.java
+++ app/src/main/java/com/example/projectmobile3/ui/myticket/MyTicketFragment.java
@@ -1,15 +1,23 @@
 package com.example.projectmobile3.ui.myticket;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.projectmobile3.R;
 import com.example.projectmobile3.databinding.FragmentMyticketBinding;
 
 public class MyTicketFragment extends Fragment {
@@ -30,5 +38,47 @@ public class MyTicketFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        LinearLayout myTicketBox = view.findViewById(R.id.ticketbox);
+        if(!getLoginStatus()){
+            myTicketBox.setVisibility(View.GONE);
+        }
+        else{
+            myTicketBox.setVisibility(View.VISIBLE);
+            myTicketBox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    showTicketDetailsPopUp();
+                }
+            });
+        }
+    }
+
+    private boolean getLoginStatus() {
+        SharedPreferences sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+        return sharedPreferences.getBoolean("is_logged_in", false);
+    }
+
+    private void showTicketDetailsPopUp(){
+        View popUpView = getLayoutInflater().inflate(R.layout.myticket_information, null);
+
+        Button btnOk = popUpView.findViewById(R.id.btnOK);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setView(popUpView);
+
+        final AlertDialog alertDialog = builder.create();
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialog.dismiss();
+            }
+        });
+
+        alertDialog.show();
+
     }
 }
